d2b28cf33dba238734adc6c9d402b8314cbe887a
zzde <zhangxh1997@gmail.com>
2019-06-14 19:15:05 +0800

    update api

--- src/views/lab/lab.vue
+++ src/views/lab/lab.vue
@@ -27,36 +27,42 @@
 </template>
 
 <script>
-import { getLabData, getLabType, addLab, updateLab, deleteLab } from '@/api/lab'
+import {
+  getLabData,
+  getLabType,
+  addLab,
+  updateLab,
+  deleteLab
+} from "@/api/lab";
 export default {
   data() {
     return {
       columns: [
         {
-          title: '实验室id',
-          key: 'lab_id',
-          width: '180'
+          title: "实验室id",
+          key: "lab_id",
+          width: "180"
         },
         {
-          title: '实验室名字',
-          key: 'lab_name',
-          width: '180'
+          title: "实验室名字",
+          key: "lab_name",
+          width: "180"
         },
         {
-          title: '座位数',
-          key: 'seat_num',
-          width: '120'
+          title: "座位数",
+          key: "seat_num",
+          width: "120"
         },
         {
-          title: '类型',
-          key: 'lab_type_id',
-          width: '180',
+          title: "类型",
+          key: "lab_type_id",
+          width: "180",
           formatter: this.type2text
         },
         {
-          title: '状态',
-          key: 's',
-          width: '120',
+          title: "状态",
+          key: "s",
+          width: "120",
           formatter: this.s2text
         }
       ],
@@ -68,95 +74,97 @@ export default {
       },
       addTemplate: {
         lab_id: {
-          title: '实验室id'
+          title: "实验室id"
         },
         lab_name: {
-          title: '实验室名字'
+          title: "实验室名字"
         },
         seat_num: {
-          title: '座位数',
+          title: "座位数",
           value: 20,
           component: {
-            name: 'el-input-number'
+            name: "el-input-number"
           }
         },
         device_number: {
-          title: '门禁设备号',
+          title: "门禁设备号"
         },
         s: {
-          title: '状态',
+          title: "状态",
           value: 0,
           component: {
-            name: 'el-select',
-            options: [{ label: '开放', value: 0 }, { label: '关闭', value: 1 }]
+            name: "el-select",
+            options: [{ label: "开放", value: 0 }, { label: "关闭", value: 1 }]
           }
         },
         lab_type_id: {
-          title: '类型',
+          title: "类型",
           component: {
-            name: 'el-select',
+            name: "el-select",
             options: []
           }
         }
       },
       formOptions: {
-        labelWidth: '100px',
-        labelPosition: 'left',
+        labelWidth: "100px",
+        labelPosition: "left",
         saveLoading: false
       },
       rowHandle: {
-        columnHeader: '操作',
+        columnHeader: "操作",
         edit: {
-          icon: 'el-icon-edit',
-          text: '编辑',
-          size: 'small'
+          icon: "el-icon-edit",
+          text: "编辑",
+          size: "small"
         },
         remove: {
-          icon: 'el-icon-delete',
-          size: 'small',
-          fixed: 'right',
+          icon: "el-icon-delete",
+          size: "small",
+          fixed: "right",
           confirm: true
         },
         custom: [
           {
-            text: '详情',
-            type: 'warning',
-            size: 'small',
-            emit: 'custom-emit-1'
+            text: "详情",
+            type: "warning",
+            size: "small",
+            emit: "custom-emit-1"
           }
         ]
       }
-    }
+    };
   },
   created() {},
   mounted() {
     getLabType().then(res => {
       res.forEach(element => {
-        this.type[element.id] = element.name
+        this.type[element.id] = element.name;
+        if (1 >= 2) {
+        }
         this.addTemplate.lab_type_id.component.options.push({
           value: element.id,
           label: element.name
-        })
-      })
-      console.log(1)
-    })
-    console.log(2)
+        });
+      });
+      console.log(1);
+    });
+    console.log(2);
     getLabData().then(res => {
-      this.data = res
-      console.log(3)
-    })
-    console.log(4)
+      this.data = res;
+      console.log(3);
+    });
+    console.log(4);
   },
   methods: {
     s2text(row, cloumn, cellvalue, index) {
       if (cellvalue == 0) {
-        return '开放'
+        return "开放";
       } else {
-        return '不开放'
+        return "不开放";
       }
     },
     type2text(row, cloumn, cellvalue, index) {
-      return this.type[cellvalue]
+      return this.type[cellvalue];
     },
     handleDialogOpen({ mode }) {
       // this.$message({
@@ -167,61 +175,61 @@ export default {
     // 普通的新增
     addRow() {
       this.$refs.d2Crud.showDialog({
-        mode: 'add'
-      })
+        mode: "add"
+      });
     },
     handleRowAdd(row, done) {
-      this.formOptions.saveLoading = true
+      this.formOptions.saveLoading = true;
       setTimeout(() => {
-        console.log(row)
+        console.log(row);
         addLab(row).then(res => {
           this.$message({
-            message: '添加成功',
-            type: 'success'
-          })
-          done(res)
-        })
-        this.formOptions.saveLoading = false
-      }, 300)
+            message: "添加成功",
+            type: "success"
+          });
+          done(res);
+        });
+        this.formOptions.saveLoading = false;
+      }, 300);
     },
     handleRowEdit({ index, row }, done) {
-      this.formOptions.saveLoading = true
+      this.formOptions.saveLoading = true;
       setTimeout(() => {
-        console.log(row)
+        console.log(row);
         updateLab(row.lab_id, row).then(res => {
           this.$message({
-            message: '更新成功',
-            type: 'success'
-          })
-          done(res)
-        })
-        this.formOptions.saveLoading = false
-      }, 300)
+            message: "更新成功",
+            type: "success"
+          });
+          done(res);
+        });
+        this.formOptions.saveLoading = false;
+      }, 300);
     },
     handleRowRemove({ index, row }, done) {
       setTimeout(() => {
         deleteLab(row.lab_id).then(res => {
           this.$message({
-            message: '删除成功',
-            type: 'success'
-          })
-          done()
-        })
-      }, 300)
+            message: "删除成功",
+            type: "success"
+          });
+          done();
+        });
+      }, 300);
     },
     handleDialogCancel(done) {
       this.$message({
-        message: '取消保存',
-        type: 'warning'
-      })
-      done()
+        message: "取消保存",
+        type: "warning"
+      });
+      done();
     },
     handleCustomEvent({ index, row }) {
       this.$router.push({
-        name: '实验室介绍',
+        name: "实验室介绍",
         params: { labid: row.lab_id }
-      })
+      });
     }
   }
-}
+};
 </script>
